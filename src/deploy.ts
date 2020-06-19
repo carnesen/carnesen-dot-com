f457481cc5cd498b384a520f446e3074b425d2c4
Chris Arnesen <chris.arnesen@gmail.com>
2020-06-19 02:21:42 -0700

    Update cli website

--- src/deploy.ts
+++ src/deploy.ts
@@ -2,42 +2,42 @@ import { runAndExit } from '@carnesen/run-and-exit';
 import { deployApp } from '@carnesen/google-cloud';
 
 runAndExit(deployApp, {
-  projectId: 'carnesen-209303',
-  zoneName: 'carnesen',
-  defaultSite: {
-    siteType: 'nodejs',
-    packageId: '@carnesen/redirector',
-  },
-  otherSites: [
-    {
-      siteType: 'static',
-      siteName: 'cli',
-      packageId: '@carnesen/cli-website',
-    },
-    {
-      siteType: 'nodejs',
-      siteName: 'www',
-      packageId: '@carnesen/www',
-    },
-    {
-      siteType: 'nodejs',
-      siteName: 'reveal-me',
-      packageId: '@carnesen/reveal-me',
-    },
-    {
-      siteType: 'nodejs',
-      siteName: 'buzz',
-      packageId: '@carnesen/buzz',
-    },
-    {
-      siteType: 'nodejs',
-      siteName: 'mathjs',
-      packageId: '@carnesen/mathjs-app',
-    },
-    {
-      siteType: 'nodejs',
-      siteName: 'meme-me',
-      packageId: '@carnesen/meme-me',
-    },
-  ],
+	projectId: 'carnesen-209303',
+	zoneName: 'carnesen',
+	defaultSite: {
+		siteType: 'nodejs',
+		packageId: '@carnesen/redirector',
+	},
+	otherSites: [
+		{
+			siteType: 'static',
+			siteName: 'cli',
+			packageId: '@carnesen/cli-website',
+		},
+		{
+			siteType: 'nodejs',
+			siteName: 'www',
+			packageId: '@carnesen/www',
+		},
+		{
+			siteType: 'nodejs',
+			siteName: 'reveal-me',
+			packageId: '@carnesen/reveal-me',
+		},
+		{
+			siteType: 'nodejs',
+			siteName: 'buzz',
+			packageId: '@carnesen/buzz',
+		},
+		{
+			siteType: 'nodejs',
+			siteName: 'mathjs',
+			packageId: '@carnesen/mathjs-app',
+		},
+		{
+			siteType: 'nodejs',
+			siteName: 'meme-me',
+			packageId: '@carnesen/meme-me',
+		},
+	],
 });
